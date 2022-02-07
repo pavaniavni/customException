2a92022ca36a98c8f11d5b58feaa89487d44710d
pavani <pavaniavni456@gmail.com>
2022-02-07 11:22:36 +0530

    added customexception

---
+++ CustomException/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException
+{
+    public class TemperatureIsZeroException : Exception
+    {
+        public TemperatureIsZeroException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class TestTemperature
+    {
+        int temp = 0;
+        public void ShowTemp()
+        {
+            if (temp == 0)
+            {
+                throw (new TemperatureIsZeroException("Zero Temperature found"));
+            }
+            else
+            {
+                Console.WriteLine("Temperature :{0}", temp);
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestTemperature testTemp = new TestTemperature();
+            try
+            {
+                testTemp.ShowTemp();
+            }
+            catch (TemperatureIsZeroException e)
+            {
+                Console.WriteLine("Custom exception is {0}", e.Message);
+            }
+        }
+    }
+}
